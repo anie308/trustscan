1a12afa1ab251dc4f428294d6943975f437f42c0
Aniebiet Obo <aniebietabasiobo@gmail.com>
2025-09-27 19:26:05 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Trust Scan',
+  description: 'TRUSTSCAN is a Depin platform that verifies the authenticity of a Product and leverages Stablecoin rewards for its loyal users.',
 }
 
 export default function RootLayout({
